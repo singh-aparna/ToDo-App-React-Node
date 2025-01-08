fcaa35850cc7748a49ce547ac98eac27965e295e
singh-aparna <singh123aparna@gmail.com>
2025-01-08 16:33:53 +0530

    Updated index.js

--- Server/index.js
+++ Server/index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 mongoose.connect(mongodb)
 
 app.get("/", (req, res) => {
-    res.send("Hello, Welcome to create your first mern stack project.")
+    res.json("Hello, Welcome to create your first mern stack project.")
 })
 
 app.get("/get", (req, res) => {
