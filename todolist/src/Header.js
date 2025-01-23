77c42e54a08c6389d36fd619aabce5b94efabb7f
singh-aparna <singh123aparna@gmail.com>
2025-01-23 22:44:42 +0530

    Updation

--- todolist/src/Header.js
+++ todolist/src/Header.js
@@ -20,7 +20,7 @@ export default function Header() {
     }, [])
     function logout() {
         //fetch('http://localhost:3001/logout', {//local
-         fetch('http://to-do-app-react-node.vercel.app/logout', {//server
+         fetch('https://to-do-app-react-node.vercel.app/logout', {//server
             credentials: 'include',
             method: "POST",
             headers: { 'Content-Type': 'application/json' },
