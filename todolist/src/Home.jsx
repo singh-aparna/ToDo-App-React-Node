2afe83ba4c57baa47568f04cc0c1b1aea039d021
singh-aparna <singh123aparna@gmail.com>
2025-01-27 00:41:18 +0530

    Updation

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -15,8 +15,8 @@ export default function Home() {
     useEffect(() => {
         axios.get("https://to-do-app-react-node.vercel.app/todos", { withCredentials: true })
             .then(response => {
-                setTodos(response);
-                console.log(response);
+                setTodos(response.data);
+                console.log(response.data);
             })
     }, []);
     if (!userInfo.username) {
@@ -28,7 +28,7 @@ export default function Home() {
             axios.post('https://to-do-app-react-node.vercel.app/todos', { task }, { withCredentials: true }) //Server
                 .then(response => {
                     console.log("Response Data:", response.data);
-                    setTodos([...todos, response.data]);
+                    setTodos(prevTodos => [...prevTodos, response.data]);
                     setTask('');
                 })
         //}
