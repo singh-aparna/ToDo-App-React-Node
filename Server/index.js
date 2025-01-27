b5ff4bbb80a8a791eb8c6ec5f1bbea5d459cdf15
singh-aparna <singh123aparna@gmail.com>
2025-01-28 00:38:38 +0530

    Updation

--- Server/index.js
+++ Server/index.js
@@ -108,7 +108,7 @@ app.get('/todos', async (req, res) => {
     }
     try {
         const payload = jwt.verify(req.cookies.token, secret); // Verify the JWT token
-        const todos = await Todo.find({ user: new mongoose.Types.ObjectId(payload.id) }).lean(); // Convert to plain objects
+        const todos = await Todo.find({ user: new mongoose.Types.ObjectId(payload.id) }) // Convert to plain objects
         res.json(todos);;
         // Send the todos as a JSON response
     } catch (err) {
